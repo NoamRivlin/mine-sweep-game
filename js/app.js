e39c75ff642ce96d1049bc7b55f86cf3292c8a0c
Noam Rivlin <88899637+NoamRivlin@users.noreply.github.com>
2022-05-26 20:37:48 +0300

    26.5.22 "submission

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@
 const FLAG = '🚩'
 const MINE = '💣'
 var gBoard
+var gStartTime
+var gIntervalId
+var isTimer = false
 
 var gGame = {
     isOn: false,
@@ -18,7 +21,6 @@ var gLevels = {
     size: 4,
     mines: 2
 }
-
 
 function init() {
     gGame.isOn = true
@@ -123,34 +125,108 @@ function renderBoard(board, selector) {
 
 
 
-// Called when a cell (td) is clicked and renders the exact cell (by data or i and j within the on click?)
+// Called when a cell (td) is clicked 
 function cellClicked(elcell, i, j) {
     if (gBoard[i][j].isShown || gBoard[i][j].isMarked) return
     gBoard[i][j].isShown = true
+    if (!gBoard[i][j].isMine) {
+        gGame.shownCount++
+    } else {
+        GameOver()
+    }
+    if (gGame.shownCount === 1 && !isTimer) {
+        startTimer()
+        isTimer = true
+    }
+    elcell.style.backgroundColor = 'green' //why isnt working
     renderBoard(gBoard, '.board-container')
-
+    checkGameOver()
 }
 
 
-// Called on right click to mark a cell (suspected to be a mine) Search the web (and implement) how to hide the context menu on right click. menu is diable
+// Called on right click to mark a cell (suspected to be a mine) Search the web (and implement) how to hide the context menu on right click. menu is diable👌
 function cellMarked(elcell, i, j) {
+    if (gBoard[i][j].isShown) return
     gBoard[i][j].isMarked = !gBoard[i][j].isMarked
+    if (gBoard[i][j].isMarked) {
+        gGame.markedCount++
+        if (gGame.markedCount === 1 & !isTimer) {
+            isTimer = true
+            startTimer()
+        }
+    } if (!gBoard[i][j].isMarked) {
+        //when removing a mark reduce the mark count
+        gGame.markedCount--
+    }
     renderBoard(gBoard, '.board-container')
+    checkGameOver()
+    console.log(gGame.markedCount);
 }
 
 
 
 // Game ends when all mines are marked, and all the other cells are shown
-// function checkGameOver() {
+//works
+function checkGameOver() {
+    // console.log((gLevels.size ** 2) - gLevels.mines);
+    // console.log(gGame.shownCount);
+    // console.log(gLevels.mines);
+    if (gGame.shownCount === ((gLevels.size ** 2) - gLevels.mines) && gGame.markedCount === gLevels.mines) {
+        console.log('over');
+        alert('👑')
+    }
+}
 
-// }
+function GameOver() {
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard.length; j++) {
+            gBoard[i][j].isShown = true
+            clearInterval(gIntervalId)
 
-
-
+        }
+    }
+}
 // When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors. NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors
 // function expandShown(board, elCell, i, j) {
 
 // }
+
+//not always outputing the right mines num
+function chooseDifficulty(level) {
+    gLevels.size = level
+    if (level === 4) {
+        gLevels.mines = 2
+    } else if (level === 8) {
+        gLevels.mines = 12
+
+    } else if (level === 12) {
+        gLevels.mines = 30
+    }
+
+    init()
+    console.log(gLevels.mines);
+    // buildBoard(gLevels.size)
+    // renderBoard(gBoard, '.board-container')
+
+    // renderBoard(gBoard, '.board-container')
+    // console.log(gBoard);
+    // startGame()
+}
+
+function startTimer() {
+    gStartTime = Date.now()
+    gIntervalId = setInterval(updateTime, 80)
+}
+
+
+function updateTime() {
+    var now = Date.now()
+    var diff = now - gStartTime
+    var secondsPast = diff / 1000
+    var elTimerSpan = document.querySelector('.timer span')
+    elTimerSpan.innerText = secondsPast.toFixed(3)
+
+}
 
 
 function getRandomInt(min, max) {
