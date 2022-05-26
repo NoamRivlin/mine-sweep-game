1ef364421077cf6a17238282d869bcb1f1647f2b
Noam Rivlin <88899637+NoamRivlin@users.noreply.github.com>
2022-05-26 17:29:26 +0300

    26.5.22 update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,158 @@
 'use strict'
+
+
+const FLAG = '🚩'
+const MINE = '💣'
+var gBoard
+
+var gGame = {
+    isOn: false,
+    shownCount: 0, //how many cells are shown
+    markedCount: 0,
+    secsPassed: 0
+}
+
+
+//todo: modifying by a button of difficulty
+var gLevels = {
+    size: 4,
+    mines: 2
+}
+
+
+function init() {
+    gGame.isOn = true
+    gBoard = buildBoard(gLevels.size)
+    placeMines(gLevels);
+    setMinesNegsCount(gBoard)
+    renderBoard(gBoard, '.board-container')
+    // console.table(gBoard)
+    console.log(gBoard)
+}
+
+
+// Builds the board. sets the cell's keys and values
+
+function buildBoard(SIZE) {
+    var board = [];
+    for (var i = 0; i < SIZE; i++) {
+        board.push([]);
+        for (var j = 0; j < SIZE; j++) {
+            var cell = {
+                minesAroundCount: 0,
+                isShown: false,
+                isMine: false,
+                isMarked: false
+            }
+            board[i][j] = cell
+        }
+    }
+    return board;
+}
+
+//placing mines in random places on the board is determined by gLevels.size and how many is determined by the gLevels.mines value 
+function placeMines(gLevels) {
+    for (var i = 0; i < gLevels.mines; i++) {
+        var randI = getRandomInt(0, gLevels.size)
+        var randj = getRandomInt(0, gLevels.size)
+        gBoard[randI][randj].isMine = true;
+    }
+}
+
+
+// Count mines around each cell and set the cell's minesAroundCount.
+function setMinesNegsCount(board) {
+    // console.log(board);
+    // going over the board
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board.length; j++) {
+            // skipping mines
+            if (board[i][j].isMine === true) continue
+            // going over neighbors
+            for (var ii = i - 1; ii <= i + 1; ii++) {
+                for (var jj = j - 1; jj <= j + 1; jj++) {
+                    // making sure it's not outside the border
+                    if (ii < 0 ||
+                        ii >= board.length ||
+                        jj < 0 ||
+                        jj >= board.length) continue;
+                    // counting mines of neighbors
+                    if (board[ii][jj].isMine) board[i][j].minesAroundCount++;
+                }
+            }
+        }
+    }
+}
+
+
+// Render the board as a <table> to the page
+// renderBoard(board)
+//data-i="${i}" data-j="${j}"  // not nessecary but maybe...
+function renderBoard(board, selector) {
+    var strHTML = '<table border="solid"><tbody>';
+
+    for (var i = 0; i < board.length; i++) {
+        strHTML += '<tr>'
+
+        for (var j = 0; j < board[0].length; j++) {
+            // var className = 'cell' class="${className}" will i need to css all td/cells?
+
+            //creating the display of the cells on DOM
+            var cellChar = '';
+            if (board[i][j].isShown)
+                if (board[i][j].isMine) {
+                    cellChar = MINE
+                } else {
+                    cellChar = board[i][j].minesAroundCount;
+
+                }
+            else if (board[i][j].isMarked) {
+                cellChar = FLAG
+            }
+            strHTML += `<td onclick="cellClicked(this, ${i}, ${j})" oncontextmenu="cellMarked(this, ${i}, ${j})"  >${cellChar}</td>`
+        }
+
+        strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>';
+
+    let elBoard = document.querySelector(selector);
+    elBoard.innerHTML = strHTML;
+
+}
+
+
+
+// Called when a cell (td) is clicked and renders the exact cell (by data or i and j within the on click?)
+function cellClicked(elcell, i, j) {
+    if (gBoard[i][j].isShown || gBoard[i][j].isMarked) return
+    gBoard[i][j].isShown = true
+    renderBoard(gBoard, '.board-container')
+
+}
+
+
+// Called on right click to mark a cell (suspected to be a mine) Search the web (and implement) how to hide the context menu on right click. menu is diable
+function cellMarked(elcell, i, j) {
+    gBoard[i][j].isMarked = !gBoard[i][j].isMarked
+    renderBoard(gBoard, '.board-container')
+}
+
+
+
+// Game ends when all mines are marked, and all the other cells are shown
+// function checkGameOver() {
+
+// }
+
+
+
+// When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors. NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors
+// function expandShown(board, elCell, i, j) {
+
+// }
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+}
--- js/util.js
+++ js/util.js
@@ -1,1 +1,177 @@
 'use strict'
+
+
+//this includes a function dont use as is
+function renderBoard(board) {
+    var strHTML = ''
+
+    for (var i = 0; i < board.length; i++) {
+        strHTML += '<tr>'
+
+        for (var j = 0; j < board[0].length; j++) {
+            // var className = 'cell' class="${className}"
+            strHTML += `<td data-i="${i}" data-j="${j}" onclick="cellClicked(this,${i} ,${j})" oncontextmenu="cellMarked(this) >${i} ,${j}</td>`
+        }
+
+        strHTML += '</tr>'
+    }
+
+
+    var elBoard = document.querySelector('tbody.board')
+    elBoard.innerHTML = strHTML
+    console.log(elBoard);
+
+    console.log(board.length);
+}
+
+
+
+
+// function buildBoard(SIZE, variable) {
+//     var board = [];
+//     for (var i = 0; i < SIZE; i++) {
+//         board.push([]);
+//         for (var j = 0; j < SIZE; j++) {
+//             var cell = variable
+//             cell.location.i = i
+//             cell.location.j = j
+//             board[i][j] = cell;
+//         }
+//     }
+//     return board;
+// }
+
+function buildBoard(SIZE) {
+    var board = [];
+    for (var i = 0; i < SIZE; i++) {
+        board.push([]);
+        for (var j = 0; j < SIZE; j++) {
+        }
+    }
+    return board;
+}
+
+function getMoveDiff() {
+    var randNum = getRandomInt(1, 5);
+    if (randNum < 2) {
+        return { i: 0, j: 1 }
+    } else if (randNum < 3) {
+        return { i: -1, j: 0 }
+    } else if (randNum < 4) {
+        return { i: 0, j: -1 }
+    } else {
+        return { i: 1, j: 0 }
+    }
+}
+
+function printMat(mat, selector) {
+    // var strHTML = '<table border="0"><tbody>';
+    var strHTML = ''
+    strHTML += '<tr>'
+    for (var i = 0; i < mat.length; i++) {
+        strHTML += '<tr>';
+        for (var j = 0; j < mat[0].length; j++) {
+            var cell = mat[i][j];
+            var className = 'cell cell-' + i + '-' + j;
+            strHTML += '<td class="' + className + '"> ' + cell + ' </td>'
+        }
+        strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>';
+    var elContainer = document.querySelector(selector);
+    elContainer.innerHTML = strHTML;
+}
+
+// Returns the class name for a specific cell
+function getClassName(location) {
+    var cellClass = 'cell-' + location.i + '-' + location.j;
+    return cellClass;
+}
+// location such as: {i: 2, j: 7}
+function renderCell(location, value) {
+    // Select the elCell and set the value
+    var elCell = document.querySelector(`.cell-${location.i}-${location.j}`);
+    elCell.innerHTML = value;
+}
+
+
+function restart() {
+    var elModal = document.querySelector('.modal')
+    elModal.style.display = 'none'
+    initGame()
+}
+
+function getEmptyCell() {
+    var emptyCells = []
+    for (var i = 1; i < gBoard.length - 1; i++) {
+        for (var j = 1; j < gBoard[0].length - 1; j++) {
+            if (!gBoard[i][j].gameElement) emptyCells.push({ i, j })
+        }
+
+    }
+    if (!emptyCells.length) displayModal(false)
+    return emptyCells.splice(getRandomInt(0, emptyCells.length), 1)[0]
+}
+
+//use for counting bombs around clicked cell revise this
+function countNeighbors(mat, rowIdx, colIdx) {
+    var count = 0
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i > mat.length - 1) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (j < 0 || j > mat[0].length - 1) continue
+            if (i === rowIdx && j === colIdx) continue
+            if (mat[i][j]) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+
+
+// left cliked reveal\right click mark-flag - needs revise
+// function handleKey(event) {
+//     var i = gGamerPos.i;   // needs to change into cellClickedPos
+//     var j = gGamerPos.j; // needs to change into cellClickedPos
+//     switch (event.key) {
+//         case 'left':
+//             moveTo(i, j - 1);
+//             break;
+//         case 'ArrowRight':
+//             moveTo(i, j + 1);
+//             break;
+//         case 'ArrowUp':
+//             moveTo(i - 1, j);
+//             break;
+//         case 'ArrowDown':
+//             moveTo(i + 1, j);
+//             break;
+//     }
+// }
+
+//min- in, max-in
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min + 1
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function cellClicked(e, i, j) {
+
+}
+
+//right click listner
+// element.addEventListener("contextmenu", function()
+
+//left click listner
+// element.addEventListener("click",
+//     function (evt) )
